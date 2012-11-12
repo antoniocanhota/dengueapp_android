acc483f2cb340d8531cea5a4a5ee8f84d9c8cd55
antoniocanhota <antoniocanhota@gmail.com>
2012-11-11 23:58:36 -0200

    Chamada da camera ao publicar den?ncia

--- gen/br/uff/antoniocanhota/dengueapp/android/R.java
+++ gen/br/uff/antoniocanhota/dengueapp/android/R.java
@@ -17,25 +17,27 @@ public final class R {
         public static final int ic_point=0x7f020003;
     }
     public static final class id {
-        public static final int bt_confirmar_publicacao_de_denuncia=0x7f070009;
+        public static final int bt_confirmar_publicacao_de_denuncia=0x7f07000b;
         public static final int bt_denuncias=0x7f070003;
         public static final int bt_dicas_prevencao=0x7f070005;
         public static final int bt_minhas_denuncias=0x7f070004;
         public static final int bt_minhas_informacoes=0x7f070006;
         public static final int bt_publicar_denuncia=0x7f070002;
         public static final int bt_sobre=0x7f070007;
+        public static final int bt_tirar_foto=0x7f07000a;
+        public static final int foto_da_denuncia=0x7f070009;
         public static final int mapa_denuncias=0x7f070000;
         public static final int mapa_minhas_denuncias=0x7f070008;
-        public static final int menu_settings=0x7f070012;
+        public static final int menu_settings=0x7f070014;
         public static final int textView1=0x7f070001;
-        public static final int textView2=0x7f07000a;
-        public static final int textView3=0x7f07000c;
-        public static final int textView4=0x7f07000e;
-        public static final int textView5=0x7f07000b;
-        public static final int textView6=0x7f07000d;
-        public static final int textView7=0x7f070010;
-        public static final int textView8=0x7f070011;
-        public static final int textView9=0x7f07000f;
+        public static final int textView2=0x7f07000c;
+        public static final int textView3=0x7f07000e;
+        public static final int textView4=0x7f070010;
+        public static final int textView5=0x7f07000d;
+        public static final int textView6=0x7f07000f;
+        public static final int textView7=0x7f070012;
+        public static final int textView8=0x7f070013;
+        public static final int textView9=0x7f070011;
     }
     public static final class layout {
         public static final int activity_denuncias=0x7f030000;
@@ -55,40 +57,41 @@ public final class R {
         public static final int activity_sobre=0x7f060005;
     }
     public static final class string {
-        public static final int aluno_email=0x7f040010;
-        public static final int aluno_label=0x7f04000f;
-        public static final int aluno_valor=0x7f04000e;
+        public static final int aluno_email=0x7f040011;
+        public static final int aluno_label=0x7f040010;
+        public static final int aluno_valor=0x7f04000f;
         /** 
 	chave para o mac: 0jThpxQvFWO9uJJsik5milTZWFAAACqh7usjHGA
 	chave para o linux de casa: 0jThpxQvFWO96kWlACMapyrB1aVjKYYiMk13WQQ
     
          */
-        public static final int api_google_maps=0x7f04001a;
+        public static final int api_google_maps=0x7f04001b;
         public static final int app_name=0x7f040000;
-        public static final int confirmar_publicacao_de_denuncia=0x7f040019;
-        public static final int denuncias=0x7f040009;
-        public static final int dicas_prevencao=0x7f04000b;
-        public static final int enviando_denuncia=0x7f040016;
+        public static final int confirmar_publicacao_de_denuncia=0x7f04001a;
+        public static final int denuncias=0x7f04000a;
+        public static final int dicas_prevencao=0x7f04000c;
+        public static final int enviando_denuncia=0x7f040017;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
-        public static final int minhas_denuncias=0x7f04000a;
-        public static final int minhas_informacoes=0x7f04001c;
-        public static final int orientador_label=0x7f040012;
-        public static final int orientador_valor=0x7f040011;
+        public static final int minhas_denuncias=0x7f04000b;
+        public static final int minhas_informacoes=0x7f04001d;
+        public static final int orientador_label=0x7f040013;
+        public static final int orientador_valor=0x7f040012;
         public static final int publicar_denuncia=0x7f040008;
-        public static final int sobre=0x7f04000c;
-        public static final int sobre_paragrafo_1=0x7f040013;
+        public static final int sobre=0x7f04000d;
+        public static final int sobre_paragrafo_1=0x7f040014;
+        public static final int tirar_foto=0x7f040009;
         public static final int title_activity_dicas_prevencao=0x7f040007;
         public static final int title_activity_main=0x7f040003;
         public static final int title_activity_minhas_denuncias=0x7f040005;
-        public static final int title_activity_minhas_informacoes=0x7f04001b;
+        public static final int title_activity_minhas_informacoes=0x7f04001c;
         public static final int title_activity_publicar_denuncia=0x7f040006;
         public static final int title_activity_sobre=0x7f040004;
-        public static final int versao_label=0x7f040015;
-        public static final int versao_valor=0x7f040014;
-        public static final int voltar_inicial=0x7f04000d;
-        public static final int webserivce_listagem_denuncias=0x7f040018;
-        public static final int webservice_publicar_denuncia=0x7f040017;
+        public static final int versao_label=0x7f040016;
+        public static final int versao_valor=0x7f040015;
+        public static final int voltar_inicial=0x7f04000e;
+        public static final int webserivce_listagem_denuncias=0x7f040019;
+        public static final int webservice_publicar_denuncia=0x7f040018;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- src/br/uff/antoniocanhota/dengueapp/android/PublicarDenunciaActivity.java
+++ src/br/uff/antoniocanhota/dengueapp/android/PublicarDenunciaActivity.java
@@ -9,26 +9,37 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class PublicarDenunciaActivity extends Activity {
-
+public class PublicarDenunciaActivity extends Activity implements OnClickListener {
+	private static final int TIRAR_FOTO = 1020394857;
+	private ImageView imgFoto;
+	private Button bt_tirar_foto;
+	
 	String webservice_de_publicar_denuncia = "http://10.0.2.2:3000/webservices/denuncias/publicar";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_publicar_denuncia);
-
+		imgFoto = (ImageView) findViewById(R.id.foto_da_denuncia);
 		Button bt_confirmar_publicacao_de_denuncia = (Button) findViewById(R.id.bt_confirmar_publicacao_de_denuncia);
+		bt_tirar_foto = (Button) findViewById(R.id.bt_tirar_foto);
 
+		bt_tirar_foto.setOnClickListener(this);
+		
 		bt_confirmar_publicacao_de_denuncia
 				.setOnClickListener(new View.OnClickListener() {
 					public void onClick(View arg0) {
@@ -49,6 +60,28 @@ public class PublicarDenunciaActivity extends Activity {
 		
 	}
 
+    // Método é subscrito para conseguir obeter o resultado da Intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Aqui verifica se o resultado é da Requisição TIRAR_FOTO.
+        if (requestCode == TIRAR_FOTO) {
+            //Ok
+            if (resultCode == RESULT_OK) {
+                // Aqui pego a imagem
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                // Seta ela no ImaView do Layout
+                imgFoto.setImageBitmap(bitmap);
+                
+                //Aqui posso salvar a foto se quizer.
+            } else if (resultCode == RESULT_CANCELED) {//Cancelou a foto
+                Toast.makeText(this, "Cancelou", Toast.LENGTH_SHORT);
+            } else { //Saiu da Intent
+                Toast.makeText(this, "Saiu", Toast.LENGTH_SHORT);
+            }
+
+        }
+    }
+	
 	private static void enviar(Context ctx, String lat, String lng) {
 		HttpClient client = new DefaultHttpClient();
 		HttpPost post = new HttpPost(ctx.getString(R.string.webservice_publicar_denuncia));
@@ -83,6 +116,18 @@ public class PublicarDenunciaActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_publicar_denuncia, menu);
 		return true;
+	}
+
+	public void onClick(View view) {
+		// Verifico se o botão clicado foi o btnIniciar
+        if (view == bt_tirar_foto) {
+            // Crio a Intent para Camera do Android
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            // Inicio a Intent como uma Intent que irá retornar algo, nesse caso
+            // a Camera, e passo um código para requisição.
+            startActivityForResult(intent, TIRAR_FOTO);
+        }
+		
 	}
 
 }
