1ef2ee90b1d3a3b8e79c3549a2fc134a5b1ad02d
antoniocanhota <antoniocanhota@gmail.com>
2013-06-13 19:28:48 -0300

    Pr?-limpeza de c?digo antigo

--- src/br/uff/antoniocanhota/dengueapp/android/PublicarDenunciaActivity.java
+++ src/br/uff/antoniocanhota/dengueapp/android/PublicarDenunciaActivity.java
@@ -11,64 +11,73 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapController;
 
 public class PublicarDenunciaActivity extends MapActivity {
+	private static final float MIN_ACCURACY = 35; // in meters
 	private static final int TIRAR_FOTO = 1020394857;
 	private ImageView imgFoto;
 	private Bitmap bitmap;
 
 	private LocationManager lm;
 	private LocationListener locationListener;
-	private MapController mapa;
-//	private String lat;
-//	private String lng;
-	
-	private Double latitude;
-	private Double longitude;
-	
-	private GeoPoint localDaDenuncia;
-	private ProgressBar progressBar;
-	
+//	private MapController mapa;
+	// private String lat;
+	// private String lng;
+
+	// private Double latitude;
+	// private Double longitude;
+	private Location userLocation;
+	// private float userLocationAccuracy = MIN_ACCURACY;
+
+	// private GeoPoint localDaDenuncia;
+//	private ProgressBar progressBar;
+
 	private Denuncia denuncia;
 
-	//static String webservice_de_publicar_denuncia = "http://dengue.herokuapp.com/webservices/denuncias/publicar";
-	//static String webservice_de_publicar_denuncia = "http://10.0.2.2:3000/webservices/denuncias/publicar";
+	// static String webservice_de_publicar_denuncia =
+	// "http://dengue.herokuapp.com/webservices/denuncias/publicar";
+	// static String webservice_de_publicar_denuncia =
+	// "http://10.0.2.2:3000/webservices/denuncias/publicar";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);		
-		progressBar = (ProgressBar) findViewById(R.id.progressBar);
-		
+		super.onCreate(savedInstanceState);
+		userLocation = null;
+
+//		progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
 		// ---use the LocationManager class to obtain locations data---
 		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		locationListener = new MyLocationListener();
+
+		
+		lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
+				locationListener);
 		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
 				locationListener);
-		
+
 		setContentView(R.layout.activity_publicar_denuncia);
 		imgFoto = (ImageView) findViewById(R.id.foto_da_denuncia);
 
 		// Centraliza e foca no local do usuГЎiro
-//		MapView mapView = (MapView) findViewById(R.id.mapa_publicar_denuncia);
-//		mapa = mapView.getController();
-//
-//		// TODO: REMOVER ESTE BACALHAU DAQUI DEPOIS
-//		if (lat != null && lng != null) {
-//			double lat_db = Double.parseDouble(lat);
-//			double lng_db = Double.parseDouble(lng);
-//			localDaDenuncia = new GeoPoint((int) (lat_db * 1E6),
-//					(int) (lng_db * 1E6));
-//
-//			mapa.animateTo(localDaDenuncia);
-//			mapa.setZoom(15);
-//		}
+		// MapView mapView = (MapView)
+		// findViewById(R.id.mapa_publicar_denuncia);
+		// mapa = mapView.getController();
+		//
+		// // TODO: REMOVER ESTE BACALHAU DAQUI DEPOIS
+		// if (lat != null && lng != null) {
+		// double lat_db = Double.parseDouble(lat);
+		// double lng_db = Double.parseDouble(lng);
+		// localDaDenuncia = new GeoPoint((int) (lat_db * 1E6),
+		// (int) (lng_db * 1E6));
+		//
+		// mapa.animateTo(localDaDenuncia);
+		// mapa.setZoom(15);
+		// }
 
 		Button bt_confirmar_publicacao_de_denuncia = (Button) findViewById(R.id.bt_confirmar_publicacao_de_denuncia);
 		ImageView bt_tirar_foto = (ImageView) findViewById(R.id.foto_da_denuncia);
@@ -83,55 +92,56 @@ public class PublicarDenunciaActivity extends MapActivity {
 		bt_confirmar_publicacao_de_denuncia
 				.setOnClickListener(new View.OnClickListener() {
 					public void onClick(View arg0) {
-						Utilitarios.showToast("Enviando denúncia...", getApplicationContext());
+						Utilitarios.showToast("Enviando denúncia...",
+								getApplicationContext());
 						buildDenuncia();
-						if (validateDenuncia() && sendDenuncia()){							
-								Toast.makeText(getApplicationContext(),
-										"Denúncia enviada com sucesso.",
-										Toast.LENGTH_SHORT).show();
-								finish();							
-						} 
-						 
-//						try {
-//							Boolean ret = PublicarDenunciaActivity.enviar(
-//									getApplicationContext(), lat, lng, bitmap);
-//							// startActivity(new
-//							// Intent("android.intent.action.MAIN"));
-//							if (ret == true) {
-//								Toast.makeText(getApplicationContext(),
-//										"DenГєncia enviada com sucesso.",
-//										Toast.LENGTH_SHORT).show();
-//								finish();
-//								
-//							} else {
-//								Toast.makeText(
-//										getApplicationContext(),
-//										"Erro ao enviar a denГєcia. Tente novamente em breve",
-//										Toast.LENGTH_SHORT).show();
-//							}
-//							lm.removeUpdates(locationListener);
-//						} catch (IOException e) {
-//							// TODO Auto-generated catch block
-//							e.printStackTrace();
-//						}
+						if (validateDenuncia() && sendDenuncia()) {
+							Toast.makeText(getApplicationContext(),
+									"Denúncia enviada com sucesso.",
+									Toast.LENGTH_SHORT).show();
+							
+						}
+						lm.removeUpdates(locationListener);
+						finish();
+						// try {
+						// Boolean ret = PublicarDenunciaActivity.enviar(
+						// getApplicationContext(), lat, lng, bitmap);
+						// // startActivity(new
+						// // Intent("android.intent.action.MAIN"));
+						// if (ret == true) {
+						// Toast.makeText(getApplicationContext(),
+						// "DenГєncia enviada com sucesso.",
+						// Toast.LENGTH_SHORT).show();
+						// finish();
+						//
+						// } else {
+						// Toast.makeText(
+						// getApplicationContext(),
+						// "Erro ao enviar a denГєcia. Tente novamente em breve",
+						// Toast.LENGTH_SHORT).show();
+						// }
+						// lm.removeUpdates(locationListener);
+						// } catch (IOException e) {
+						// // TODO Auto-generated catch block
+						// e.printStackTrace();
+						// }
 					}
 				});
 
 	}
-	
-	private void buildDenuncia(){
+
+	private void buildDenuncia() {
 		this.denuncia = new Denuncia();
-		denuncia.setLatitude(latitude);
-		denuncia.setLongitude(longitude);
+		denuncia.setLatitude(userLocation.getLatitude());
+		denuncia.setLongitude(userLocation.getLongitude());
 		denuncia.setFoto(bitmap, getApplicationContext());
 	}
-	
-	private boolean validateDenuncia(){
+
+	private boolean validateDenuncia() {
 		boolean result = true;
-		if (denuncia.getFoto() == null){
+		if (denuncia.getFoto() == null) {
 			result = false;
-			Toast.makeText(
-					getApplicationContext(),
+			Toast.makeText(getApplicationContext(),
 					"Você precisa tirar uma foto do local antes de publicar!",
 					Toast.LENGTH_SHORT).show();
 		}
@@ -144,58 +154,85 @@ public class PublicarDenunciaActivity extends MapActivity {
 		}
 		return result;
 	}
-	
-	private boolean sendDenuncia(){
+
+	private boolean sendDenuncia() {
 		Webservice ws = new Webservice(getApplicationContext());
 		return ws.postDenuncia(denuncia);
 	}
-	
+
 	private class MyLocationListener implements LocationListener {
+
 		public void onLocationChanged(Location loc) {
+
 			if (loc != null) {
-				//Geocoder geoCoder = new Geocoder(getBaseContext(), Locale.getDefault());
-				TextView addressView = (TextView) findViewById(R.id.endereco_aproximado_da_denuncia);
-				addressView.setText("LocalizaГ§ГЈo obtida com sucesso!\n\n"+"Latitude: "+loc.getLatitude()+"\n"+"Longitude:"+loc.getLongitude());
-				
-//				try {
-//					List<Address> addresses = geoCoder.getFromLocation(
-//							loc.getLatitude(),
-//							loc.getLongitude(),
-//							1);
-//					String add = "";
-//					if (addresses.size() > 0){						
-//						for (int i=0; i<addresses.get(0).getMaxAddressLineIndex(); i++)
-//							add += addresses.get(0).getAddressLine(i) + "\n";
-//					}					
-//					if (add == "") {
-//						addressView.setText("Latitude: "+loc.getLatitude()+"\n"+"Longitude:"+loc.getLongitude());
-//					}else{
-//						addressView.setText(add);
-//					}							
-//				} catch (IOException e) {
-//					// TODO Auto-generated catch block
-//					addressView.setText("Latitude: "+loc.getLatitude()+"\n"+"Longitude:"+loc.getLongitude());
-//					e.printStackTrace();
-//				}
+
+				if ((userLocation != null && loc.getAccuracy() <= userLocation
+						.getAccuracy())
+						|| (userLocation == null && loc.getAccuracy() <= MIN_ACCURACY)) {
+					
+					userLocation = loc;
+					TextView addressView = (TextView) findViewById(R.id.endereco_aproximado_da_denuncia);
+					addressView.setText("Localização obtida com sucesso!\n\n"
+							+ "Latitude: " + loc.getLatitude() + "\n"
+							+ "Longitude:" + loc.getLongitude());
+					
+				}
 			}
-			localDaDenuncia = new GeoPoint((int) (loc.getLatitude()),
-					(int) (loc.getLongitude()));
+
+			if (loc != null && loc.getAccuracy() <= MIN_ACCURACY
+					&& loc.getAccuracy() <= userLocation.getAccuracy()) {
+
+				// Geocoder geoCoder = new Geocoder(getBaseContext(),
+				// Locale.getDefault());
+//				TextView addressView = (TextView) findViewById(R.id.endereco_aproximado_da_denuncia);
+//				addressView.setText("LocalizaГ§ГЈo obtida com sucesso!\n\n"
+//						+ "Latitude: " + loc.getLatitude() + "\n"
+//						+ "Longitude:" + loc.getLongitude());
+
+				// try {
+				// List<Address> addresses = geoCoder.getFromLocation(
+				// loc.getLatitude(),
+				// loc.getLongitude(),
+				// 1);
+				// String add = "";
+				// if (addresses.size() > 0){
+				// for (int i=0; i<addresses.get(0).getMaxAddressLineIndex();
+				// i++)
+				// add += addresses.get(0).getAddressLine(i) + "\n";
+				// }
+				// if (add == "") {
+				// addressView.setText("Latitude: "+loc.getLatitude()+"\n"+"Longitude:"+loc.getLongitude());
+				// }else{
+				// addressView.setText(add);
+				// }
+				// } catch (IOException e) {
+				// // TODO Auto-generated catch block
+				// addressView.setText("Latitude: "+loc.getLatitude()+"\n"+"Longitude:"+loc.getLongitude());
+				// e.printStackTrace();
+				// }
+			}
+
+			// localDaDenuncia = new GeoPoint((int) (loc.getLatitude()),
+			// (int) (loc.getLongitude()));
 			// p = new GeoPoint((int) (loc.getLatitude() * 1E6),(int)
 			// (loc.getLongitude() * 1E6));
 			// mc.animateTo(p);
 			// mc.setZoom(18);
 
-			latitude = loc.getLatitude();
-			longitude = loc.getLongitude();
-			
-//			lat = String.valueOf((loc.getLatitude()));
-//			lng = String.valueOf((loc.getLongitude()));
+//			userLocation = loc;
+			// latitude = loc.getLatitude();
+			// longitude = loc.getLongitude();
+
+			// lat = String.valueOf((loc.getLatitude()));
+			// lng = String.valueOf((loc.getLongitude()));
 		}
 
 		public void onProviderDisabled(String provider) {
+			Utilitarios.showToast("Por favor, reative o sensor GPS para o correto funcionamento do DengueApp. Pode ser necessário reiniciar a aplicação.",
+					getApplicationContext());
 		}
 
-		public void onProviderEnabled(String provider) {
+		public void onProviderEnabled(String provider) {//			
 		}
 
 		public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -211,78 +248,78 @@ public class PublicarDenunciaActivity extends MapActivity {
 				imgFoto.setImageBitmap(bitmap);
 				// A foto poderГЎ ser salva aqui
 			} else if (resultCode == RESULT_CANCELED) {
-				Toast.makeText(this, "Cancelou", Toast.LENGTH_SHORT);
+				Toast.makeText(this, "Cancelou", Toast.LENGTH_SHORT).show();
 			} else {
-				Toast.makeText(this, "Saiu", Toast.LENGTH_SHORT);
+				Toast.makeText(this, "Saiu", Toast.LENGTH_SHORT).show();
 			}
 
 		}
 	}
 
-//	private static boolean enviar(Context ctx, String lat, String lng,
-//			Bitmap imagem) throws IOException {
-//
-//		Boolean resultado = false;
-//		if (imagem == null) {
-//			Toast.makeText(ctx,
-//					"VocГЄ precisa tirar uma foto do local antes de publicar!",
-//					Toast.LENGTH_SHORT).show();
-//		} else {
-//			if (lat == null || lng == null) {
-//				Toast.makeText(
-//						ctx,
-//						"Ainda nГЈo conseguimos obter sua localizaГ§ГЈo. Aguarde alguns segundos e tente novamente.",
-//						Toast.LENGTH_SHORT).show();
-//			} else {
-//
-//				HttpClient client = new DefaultHttpClient();
-//				HttpPost post = new HttpPost(
-//						webservice_de_publicar_denuncia);
-//				MultipartEntity mpEntity = new MultipartEntity(
-//						HttpMultipartMode.BROWSER_COMPATIBLE);
-//
-//				// Object filename;
-//				// create a file to write bitmap data
-//				File f = new File(ctx.getCacheDir(), "foto_denuncia.jpg");
-//				f.createNewFile();
-//
-//				// Convert bitmap to byte array
-//				ByteArrayOutputStream bos = new ByteArrayOutputStream();
-//				imagem.compress(CompressFormat.PNG, 0 /* ignored for PNG */, bos);
-//				byte[] bitmapdata = bos.toByteArray();
-//
-//				// write the bytes in file
-//				FileOutputStream fos = new FileOutputStream(f);
-//				fos.write(bitmapdata);
-//				fos.flush();
-//				fos.close();
-//
-//				// adiciona os parametros Г  requisiГ§ГЈo POST
-//				mpEntity.addPart("denuncia[foto]",
-//						new FileBody(f, "image/jpeg"));
-//				mpEntity.addPart("denuncia[latitude]", new StringBody(lat,
-//						Charset.forName("UTF-8")));
-//				mpEntity.addPart("denuncia[longitude]", new StringBody(lng,
-//						Charset.forName("UTF-8")));
-//				
-//				mpEntity.addPart(
-//						"dispositivo[identificador_do_android]",
-//						new StringBody(Utilitarios.getAndroidID(ctx), Charset
-//								.forName("UTF-8")));				
-//				try {
-//					post.setEntity(mpEntity);
-//					HttpResponse response = client.execute(post);
-//					resultado = true;
-//				} catch (Exception e) {
-//					// TODO Auto-generated catch block
-//					resultado = false;
-//					e.printStackTrace();
-//				}
-//			}
-//		}
-//		return resultado;
-//
-//	}
+	// private static boolean enviar(Context ctx, String lat, String lng,
+	// Bitmap imagem) throws IOException {
+	//
+	// Boolean resultado = false;
+	// if (imagem == null) {
+	// Toast.makeText(ctx,
+	// "VocГЄ precisa tirar uma foto do local antes de publicar!",
+	// Toast.LENGTH_SHORT).show();
+	// } else {
+	// if (lat == null || lng == null) {
+	// Toast.makeText(
+	// ctx,
+	// "Ainda nГЈo conseguimos obter sua localizaГ§ГЈo. Aguarde alguns segundos e tente novamente.",
+	// Toast.LENGTH_SHORT).show();
+	// } else {
+	//
+	// HttpClient client = new DefaultHttpClient();
+	// HttpPost post = new HttpPost(
+	// webservice_de_publicar_denuncia);
+	// MultipartEntity mpEntity = new MultipartEntity(
+	// HttpMultipartMode.BROWSER_COMPATIBLE);
+	//
+	// // Object filename;
+	// // create a file to write bitmap data
+	// File f = new File(ctx.getCacheDir(), "foto_denuncia.jpg");
+	// f.createNewFile();
+	//
+	// // Convert bitmap to byte array
+	// ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	// imagem.compress(CompressFormat.PNG, 0 /* ignored for PNG */, bos);
+	// byte[] bitmapdata = bos.toByteArray();
+	//
+	// // write the bytes in file
+	// FileOutputStream fos = new FileOutputStream(f);
+	// fos.write(bitmapdata);
+	// fos.flush();
+	// fos.close();
+	//
+	// // adiciona os parametros Г  requisiГ§ГЈo POST
+	// mpEntity.addPart("denuncia[foto]",
+	// new FileBody(f, "image/jpeg"));
+	// mpEntity.addPart("denuncia[latitude]", new StringBody(lat,
+	// Charset.forName("UTF-8")));
+	// mpEntity.addPart("denuncia[longitude]", new StringBody(lng,
+	// Charset.forName("UTF-8")));
+	//
+	// mpEntity.addPart(
+	// "dispositivo[identificador_do_android]",
+	// new StringBody(Utilitarios.getAndroidID(ctx), Charset
+	// .forName("UTF-8")));
+	// try {
+	// post.setEntity(mpEntity);
+	// HttpResponse response = client.execute(post);
+	// resultado = true;
+	// } catch (Exception e) {
+	// // TODO Auto-generated catch block
+	// resultado = false;
+	// e.printStackTrace();
+	// }
+	// }
+	// }
+	// return resultado;
+	//
+	// }
 
 	protected boolean isRouteDisplayed() {
 		// TODO Auto-generated method stub return false;
